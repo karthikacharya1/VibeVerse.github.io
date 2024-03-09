0be18ab699a1b29502ccefbf11d13f7250b3de5f
Karthik Acharya <129307817+karthikacharya1@users.noreply.github.com>
2024-03-09 22:03:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ async function getSongs(folder) {
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split(`/${folder}/`)[1]);
+      songs.push(element.href.split(`https://karthikacharya1.github.io/VibeVerse/${folder}/`)[1]);
     }
   }
 
